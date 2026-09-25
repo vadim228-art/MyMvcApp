414f1957abc40e06685954f73a556ce7c687a678
Raze <toosvalet@gmail.com>
2026-09-25 16:38:52 +0400

    вроде сделал, но это не точно

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,19 +6,37 @@ namespace MyMvcApp.Controllers;
 
 public class HomeController : Controller
 {
+    // GET: показываем пустую форму
     public IActionResult Index()
     {
         return View();
     }
 
-    public IActionResult Privacy()
+    // POST: принимаем данные из формы и отдаём обратно
+    [HttpPost]
+    public IActionResult Index(Book book)
     {
-        return View();
+        return View(book);
     }
+    
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
         return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+
+    // GET: показываем пустую форму
+    public IActionResult Books()
+    {
+        return View();
+    }
+
+    // POST: получаем данные из формы и передаём их обратно в View
+    [HttpPost]
+    public IActionResult Books(Book book)
+    {
+        return View(book);
     }
 }
---
+++ Models/Book.cs
@@ -0,0 +1,8 @@
+namespace MyMvcApp.Models
+{
+    public class Book
+    {
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+    }
+}
