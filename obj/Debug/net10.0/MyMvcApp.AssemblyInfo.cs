c46f3c3dc9c52614cac45a5e4283bac37799f4b9
Raze <toosvalet@gmail.com>
2026-09-25 15:14:31 +0400

    Обновил информацию на главной странице и убрал лишнее

--- obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
+++ obj/Debug/net10.0/MyMvcApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2c8a77cc5432d58e200e85139164481f4a578e48")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyMvcApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
